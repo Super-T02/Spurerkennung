8298bb6f364f73f04036ed01a4ff6841d7eb04d2
Super-T02 <75214791+Super-T02@users.noreply.github.com>
2022-11-21 15:36:38 +0100

    -ADD: Hug lines to python file

--- src/main.py
+++ src/main.py
@@ -16,7 +16,7 @@ class Main():
         self.calib = cal.Calibration(False)
         self.path = path
         
-    def startVideo(self):
+    def startVideo(self, hugs=False):
         if not os.path.exists(self.path):
             return print('Video not found')
 
@@ -34,21 +34,22 @@ class Main():
                 break
             
             # Do here the image processing
-            frame = self._preprocess(frame)
+            frame = self._preprocess(frame, hugs=hugs)
 
             # Do operations on the frame
             gray = frame
-            gray = cv.resize(gray, (self.WIN_X, self.WIN_Y))
-            font = cv.FONT_HERSHEY_SIMPLEX
-            new_frame_time = time.time()
+            if gray is not None:
+                gray = cv.resize(gray, (self.WIN_X, self.WIN_Y))
+                font = cv.FONT_HERSHEY_SIMPLEX
+                new_frame_time = time.time()
 
-            # Calculate Frame Rate
-            fps, prev_frame_time = self._calcFPS(prev_frame_time, new_frame_time)
+                # Calculate Frame Rate
+                fps, prev_frame_time = self._calcFPS(prev_frame_time, new_frame_time)
 
-            # Put fps on the screen
-            cv.putText(gray, fps, (7, 21), font, 1, (100, 100, 100), 2, cv.LINE_AA)
+                # Put fps on the screen
+                cv.putText(gray, fps, (7, 21), font, 1, (100, 100, 100), 2, cv.LINE_AA)
 
-            cv.imshow('Video', gray)
+                cv.imshow('Video', gray)
 
             # press 'Q' for exit
             if cv.waitKey(1) & 0xFF == ord('q'):
@@ -68,21 +69,27 @@ class Main():
 
         return fps, prev_frame_time
 
-    def _preprocess(self, img):
+    def _preprocess(self, img, hugs=False):
         # Equalize the image
         img = self.calib.equalize(img)
-        
+        self.equilized_img = img
+
         # Convert to grayscale
         img = cv.cvtColor(img, cv.COLOR_RGB2GRAY)
 
         # Apply Canny edge detection
-        edges = cv.Canny(img, 100, 150)
+        img = cv.Canny(img, 100, 150)
 
         # Segmentation of the image
-        edges = self._segmentation(edges)
+        img = self._segmentation(img)
+
+        # Get the hough lines
+        if hugs:
+            lines = self._getHoughLines(img)
+            img = self._drawLines(self.equilized_img, lines)
 
 
-        return edges
+        return img
 
     def _segmentation(self, img):
         # Define a blank matrix that matches the image height/width.
@@ -111,6 +118,37 @@ class Main():
 
         return roi
 
+    def _drawLines(self, img, lines, color=[0,0,255], thickness=10):
+        # If there are no lines to draw, exit.
+        if lines is None:
+            return    # Make a copy of the original image.
+        img = np.copy(img)    # Create a blank image that matches the original in size.
+        line_img = np.zeros(
+            (
+                img.shape[0],
+                img.shape[1],
+                3
+            ),
+            dtype=np.uint8,
+        )    # Loop over all lines and draw them on the blank image.
+        for line in lines:
+            for x1, y1, x2, y2 in line:
+                cv.line(line_img, (x1, y1), (x2, y2), color, thickness)    # Merge the image with the lines onto the original.
+        img = cv.addWeighted(img, 0.8, line_img, 1.0, 0.0)    # Return the modified image.
+        return img
+
+    def _getHoughLines(self, img):
+        lines = cv.HoughLinesP(
+            img,
+            rho=6,
+            theta=np.pi / 60,
+            threshold=160,
+            lines=np.array([]),
+            minLineLength=40,
+            maxLineGap=20
+        )
+        return lines
+
 
 if __name__ == '__main__':
     # Path to video
@@ -124,6 +162,9 @@ if __name__ == '__main__':
     main2 = Main(videoHardest)
 
     main.startVideo()
+    main.startVideo(True)
     main1.startVideo()
+    main1.startVideo(True)
     main2.startVideo()
+    main2.startVideo(True)
     
